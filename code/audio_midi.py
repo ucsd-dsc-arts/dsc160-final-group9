71e90d88880eee4fc77558625da98a8ec6d8e828
Connie <connielee99@gmail.com>
2020-06-04 22:15:18 +0800

    audio ipynb py

---
+++ code/audio_midi.py
@@ -0,0 +1,152 @@
+# links to videos for download
+urls = [
+    # 这里是按照网易云排行找的
+
+    # 重生
+    'https://www.youtube.com/watch?v=Yr2EPJxAXlQ',
+    # 一千零一次我爱你
+    'https://www.youtube.com/watch?v=VqKRYpArttI',
+    # 女孩你为何踮脚尖
+    'https://www.bilibili.com/video/av41048045/',
+    # 梨渦淺笑
+    'https://www.youtube.com/watch?v=oIjmyGo3Rfo',
+    # 喜欢你
+    'https://www.youtube.com/watch?v=WbHtPhoy3XI',
+    # 过活
+    'https://www.bilibili.com/video/BV1V5411s7sE?from=search&seid=17605434968373175503',
+    # 不再等候的姑娘
+    'https://www.bilibili.com/video/BV1ja4y1x7eX?from=search&seid=9393599810489686433',
+    # 最天使
+    'https://www.youtube.com/watch?v=CjABkVCrVKo',
+    # MOM
+    'https://www.youtube.com/watch?v=y5YiXMJD2rA',
+    # Manta 00:30 - 02:54
+    'https://www.bilibili.com/video/av95042894/',
+    # 处处吻
+    'https://www.youtube.com/watch?v=HZdf1u5G2SQ',
+    # last dance
+    'https://www.youtube.com/watch?v=GBEzWeEy_E8',
+    # 想见你想见你想见你 
+    'https://www.youtube.com/watch?v=pRsVfFmWfDI',
+    # 蓝
+    'https://www.youtube.com/watch?v=zUwrdxfM4CY',
+    # 来不及
+    'https://www.youtube.com/watch?v=jD9j5u5eIlM',
+    # 你要相信這不是最後一天
+    'https://www.youtube.com/watch?v=3bWLJ8ZtP3U',
+    # 你说
+    'https://www.youtube.com/watch?v=sIjo_DTGKLQ',
+    # 岁岁平安
+    'https://www.youtube.com/watch?v=-CRMGdRLVM8',
+    # 你的答案
+    'https://www.youtube.com/watch?v=lfMXgl72pNc',
+    # 透明
+    'https://www.youtube.com/watch?v=S2s6jH_oxTI',
+    # 心似烟火
+    'https://www.bilibili.com/video/BV1N741167Cu?from=search&seid=4720747501922453686',
+    # 和你的每一天
+    'https://www.youtube.com/watch?v=uHSnI3fVRv4',
+    # 没有理由
+    'https://www.bilibili.com/video/BV1eJ411z7Kc?from=search&seid=9946292332180005403',
+    # 下山
+    'https://www.youtube.com/watch?v=aPWPf1GHIV0',
+    # 芒种
+    'https://www.youtube.com/watch?v=QwO_DN5H_Ng',
+    # 你保护世界，我保护你 00:27 -
+    'https://www.bilibili.com/video/av73152766/',
+    # 野狼disco 00:14 -
+    'https://www.youtube.com/watch?v=VWZEXb8Cibw',
+    # 关山酒
+    'https://www.bilibili.com/video/av94179915/',
+    # 我願意平凡的陪在你身旁
+    'https://www.youtube.com/watch?v=eBnGHLu-Lys',
+    # 爱你3000
+    'https://www.bilibili.com/video/av64772589/',
+    # 无关于你
+    'https://www.bilibili.com/video/av77035861/',
+    
+    # 从这里开始是自己找的
+    
+    # 晚安 00:12 -
+    'https://www.youtube.com/watch?v=OxKEwubUerk',
+    # 多想在平庸的生活擁抱你
+    'https://www.youtube.com/watch?v=9RwUNeVhAJ8',
+    # 绿色
+    'https://www.youtube.com/watch?v=wOdeNJqs4gw',
+    # 佛系少女 00:12 -
+    'https://www.youtube.com/watch?v=Z-DA·r00RmL0',
+    # 除了春天 爱情和樱花
+    'https://www.youtube.com/watch?v=iAaj4xNqwp8',
+    # 123我愛你 00:12 -
+    'https://www.youtube.com/watch?v=janCplvIE-M',
+    # 沙漠骆驼
+    'https://www.youtube.com/watch?v=u9AtUNal20A',
+    # 我在人民广场吃炸鸡
+    'https://www.youtube.com/watch?v=qtZa1Ppmuv0',
+    # PPAP
+    'https://www.youtube.com/watch?v=jQMQ_hq53vk',
+    # 追光者
+    'https://www.youtube.com/watch?v=0b7HPZ-FJ80',
+    # 剛好遇見你
+    'https://www.youtube.com/watch?v=ZjoHbkBnV3k',
+    # MKJ - Time
+    'https://www.youtube.com/watch?v=Zs2iUOk3xEQ',
+    # 全部都是你
+    'https://www.youtube.com/watch?v=KlXGbKtQ8S0',
+    # 卡路里 
+    'https://www.youtube.com/watch?v=33cwmI3YPhU',
+    # 答案 00:14 -
+    'https://www.youtube.com/watch?v=w0bXWGO7D7I',
+    # 学猫叫 00:13 -
+    'https://www.youtube.com/watch?v=mvTckqIbuLA',
+    # 空空如也 00:12 -
+    'https://www.youtube.com/watch?v=w8PICylnU2k',
+    # 不仅仅是喜欢 00:12 -
+    'https://www.youtube.com/watch?v=u2R0CCJobfk',
+    # 往后余生
+    'https://www.youtube.com/watch?v=JnmMFla6Q40',
+    # 38度6
+    'https://www.youtube.com/watch?v=BbqJ88VeNF8',
+    # 讲真的
+    'https://www.youtube.com/watch?v=QsemLBMrilQ',
+    # 可能否
+    'https://www.youtube.com/watch?v=h-e3JMYZxHo',
+    # 爱你
+    'https://www.youtube.com/watch?v=inx9U1JMS7o',
+    # 後來遇見他
+    'https://www.youtube.com/watch?v=R4eCQS9AQQ8',
+    # 像鱼
+    'https://www.youtube.com/watch?v=MzGghu_iVVk',
+    # 過客
+    'https://www.youtube.com/watch?v=pBl2Pyy_h2g',
+    # 疑心病
+    'https://www.youtube.com/watch?v=9RqPm6ZP7VM',
+    # 9420
+    'https://www.youtube.com/watch?v=M8tba4zHYtU',
+    # 病变
+    'https://www.youtube.com/watch?v=iai47zeV3Mo',
+    # 可不可以
+    'https://www.youtube.com/watch?v=5eoZDDS8MtE',
+    # 体面
+    'https://www.youtube.com/watch?v=BRJVXC7VvZk',
+    # 最美的期待
+    'https://www.youtube.com/watch?v=7J_me76geDU',
+    # 纸短情长
+    'https://www.youtube.com/watch?v=GVsZ5F5l1Uc',
+    # 大田後生仔
+    'https://www.youtube.com/watch?v=RCcERcjpLyw',
+    # 你的酒馆对我打了烊
+    'https://www.youtube.com/watch?v=Dq9t6hXWDHI',
+    # 告白气球
+    'https://www.youtube.com/watch?v=TDM6x-VJJSw',
+    # 醉赤壁
+    'https://www.youtube.com/watch?v=u70_-4j3qUg',
+    # 再遇不到你这样的人
+    'https://www.youtube.com/watch?v=EBJsTVVHrlg',
+    # 说散就散
+    'https://www.youtube.com/watch?v=MPfoPkJb0BI',
+    # 夏天的风
+    'https://www.youtube.com/watch?v=K5qf1YZ9SYE',
+    # 那女孩對我說
+    'https://www.youtube.com/watch?v=gGYz7qS80Ms'
+]
